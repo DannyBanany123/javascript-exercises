f0a8dadf843322308b577102c0db13abbbb2a09e
DannyBanany123 <ngodanny2@gmail.com>
2022-08-13 02:55:58 -0500

    Complete exercise 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let bank = "abcdefghijklmnopqrstuvwxyz";
+    let word = string.toLowerCase();
+    let a = "";
+    let b = "";
+    for (let i = 0; i < word.length; i++) {
+        if (bank.includes(word.charAt(i))) {
+            a += word.charAt(i);
+        }
+    }
+    for (let i = word.length - 1; i >= 0; i--) {
+        if (bank.includes(word.charAt(i))) {
+            b += word.charAt(i);
+        }
+    }
+    console.log(a);
+    console.log(b);
+    return a === b;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let init = calculateAge(array[0]);
+    let index = 0;
+    for (let i = 0; i < array.length; i++) {
+        let age = calculateAge(array[i]);
+        if (age > init) {
+            index = i;
+            init = age;
+        }
+    }
+    return array[index];
 };
+
+function calculateAge(obj) {
+    let age;
+    if (!("yearOfDeath" in obj)) {
+        age = 2022 - obj.yearOfBirth;
+    } else {
+        age = obj.yearOfDeath - obj.yearOfBirth;
+    }
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
